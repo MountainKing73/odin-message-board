9d338d8cb6ddb4da0a005bb1138c9503a01f2360
James Cummings <jamescummings@gmail.com>
2024-10-27 15:37:49 -0400

    added new message and message detail

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const app = express();
 const path = require("node:path");
+
 const indexRouter = require("./routes/indexRouter");
-const newRouter = require("./routes/newRouter");
 
 const assetsPath = path.join(__dirname, "public");
 app.use(express.static(assetsPath));
+
+app.use(express.urlencoded({ extended: true }));
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use("/", indexRouter);
-app.use("/new", newRouter);
 
 const PORT = 8080;
 app.listen(PORT, () => {
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -19,4 +19,25 @@ indexRouter.get("/", (req, res) => {
   res.render("index", { messages: messages });
 });
 
+indexRouter.get("/new", (req, res) => {
+  res.render("form");
+});
+
+indexRouter.get("/message/:msgId", (req, res) => {
+  const { msgId } = req.params;
+  res.render("message", { message: messages[msgId] });
+});
+
+indexRouter.post("/new", (req, res) => {
+  console.log("message: " + req.body.message);
+  console.log("author: " + req.body.author);
+  messages.push({
+    text: req.body.message,
+    user: req.body.author,
+    added: new Date(),
+  });
+
+  res.redirect("/");
+});
+
 module.exports = indexRouter;
